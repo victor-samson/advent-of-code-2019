558d8732163d1954e9d2d7518ae4e551539cbb52
Victor Samson <victor.samson@zuuse.com>
2019-12-03 22:11:02 +1100

    Add D1P2

--- advent-of-code/Program.cs
+++ advent-of-code/Program.cs
@@ -8,8 +8,10 @@ namespace advent_of_code
         static void Main(string[] args)
         {
             var d1p1 = new D1P1();
-            Console.WriteLine($"Day 1 Puzzle 1: {d1p1.Result}");
+            Console.WriteLine($"D1P1: {d1p1.Result}");
 
+            var d1p2 = new D1P2();
+            Console.WriteLine($"D1P2: {d1p2.Result}");
 
             Console.ReadLine();
         }
---
+++ advent-of-code/Puzzles/D1P2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace advent_of_code.Puzzles
+{
+    public class D1P2 : IPuzzle
+    {
+        public int Result { get; private set; }
+        private List<decimal> _masses { get; set; }
+
+        public D1P2()
+        {
+            _masses = new List<decimal>();
+            ReadPuzzleInputFromFile();
+            CalculateAnswer();
+        }
+
+        public void CalculateAnswer()
+        {
+            var result = 0;
+            foreach (var mass in _masses)
+            {
+                result += CalculateRecursiveFuelRequired(mass);
+            }
+            Result = result;
+        }
+
+        public void ReadPuzzleInputFromFile()
+        {
+            string line;
+            using (var file = new StreamReader(@"c:\victor\advent-of-code-2019\advent-of-code\Inputs\D1P2.txt"))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    _masses.Add(Decimal.Parse(line));
+                }
+            }
+        }
+
+        public static int CalculateRecursiveFuelRequired(decimal mass)
+        {
+            var result = 0;
+            while (mass > 0)
+            {
+                var fuel = CalculateFuelRequired(mass);
+                if (fuel > 0)
+                {
+                    result += fuel;
+                }
+                mass = fuel;
+            }
+            return result;
+        }
+
+        private static int CalculateFuelRequired(decimal mass)
+        {
+            return (int)Math.Truncate(mass / 3) - 2;
+        }
+    }
+}
