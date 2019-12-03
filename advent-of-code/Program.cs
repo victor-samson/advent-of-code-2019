4b29cdf458d43897ffbdcded6871279b7353de97
Victor Samson <victor.samson@zuuse.com>
2019-12-03 12:51:29 +1100

    Add solution and project

---
+++ advent-of-code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace advent_of_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
