7ad85a86c14e288b4a2abb24e7b87f8e00237bda
Victor Samson <victor.samson@zuuse.com>
2019-12-03 22:11:28 +1100

    Add unit tests

---
+++ advent-of-code-tests/Days/D1P1Tests.cs
@@ -0,0 +1,23 @@
+﻿using advent_of_code.Puzzles;
+using NUnit.Framework;
+
+namespace advent_of_code_tests.Days
+{
+    [TestFixture]
+    public class D1P1Tests
+    {
+        [TestCase(12, 2)]
+        [TestCase(14, 2)]
+        [TestCase(1969, 654)]
+        [TestCase(100756, 33583)]
+        public void CalculateFuelRequired(decimal mass, int expected)
+        {
+            // Arrange.
+            // Act.
+            var result = D1P1.CalculateFuelRequired(mass);
+
+            // Assert.
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ advent-of-code-tests/Days/D1P2Tests.cs
@@ -0,0 +1,22 @@
+﻿using advent_of_code.Puzzles;
+using NUnit.Framework;
+
+namespace advent_of_code_tests.Days
+{
+    [TestFixture]
+    public class D1P2Tests
+    {
+        [TestCase(14, 2)]
+        [TestCase(1969, 966)]
+        [TestCase(100756, 50346)]
+        public void CalculateFuelRequired(decimal mass, int expected)
+        {
+            // Arrange.
+            // Act.
+            var result = D1P2.CalculateRecursiveFuelRequired(mass);
+
+            // Assert.
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
