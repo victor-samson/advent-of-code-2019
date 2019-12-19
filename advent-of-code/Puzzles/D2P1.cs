e659b1636cc7df5e2ad5f20176db51a831a72f53
Victor Samson <victor.samson@zuuse.com>
2019-12-19 22:46:35 +1100

    WIP Day 2 Puzzle 1

---
+++ advent-of-code/Puzzles/D2P1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace advent_of_code.Puzzles
+{
+    public class D2P1 : IPuzzle
+    {
+        public void CalculateAnswer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadPuzzleInputFromFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
